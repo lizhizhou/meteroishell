5ba985c0c90ce96de9915fe83714671198aaace8
lizhizhou <lzz@meteroi.com>
2014-07-02 05:35:12 -0700

    add api file
    Signed-off-by: lizhizhou <lzz@meteroi.com>

---
+++ api/lophilo.h
@@ -0,0 +1,13 @@
+/*
+ * lophilo.h
+ *
+ *  Created on: Jun 14, 2014
+ *      Author: zhizhouli
+ */
+
+#ifndef LOPHILO_H_
+#define LOPHILO_H_
+extern void led(int id, char r, char g, char b);
+extern void io(int id, int value);
+
+#endif /* LOPHILO_H_ */
