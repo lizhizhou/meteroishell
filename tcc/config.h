a8f5ae42b646c310e5445e51d7432868c8ca4b97
lizhizhou <lzz@meteroi.com>
2014-10-28 07:13:35 -0700

    use armv4 softfp

--- tcc/config.h
+++ tcc/config.h
@@ -4,9 +4,8 @@
 #endif
 #define GCC_MAJOR 4
 #define HOST_ARM 1
+#define TCC_ARM_VERSION 4
 #define TCC_VERSION "0.9.25"
-#define TCC_ARM_EABI 1
 #define FORTIFY_SOURCE 0
 #define TCC_TARGET_ARM 1
-#define TCC_ARM_EABI 1
 #define WITHOUT_LIBTCC 1
